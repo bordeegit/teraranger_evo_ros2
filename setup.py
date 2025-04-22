5ab198f4707d1f94ba096c860b8c979d99c1d956
Matteo Bordignon <matteo.bordignon@yahoo.it>
2025-04-22 18:08:43 +0200

    Changed Licencing

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     maintainer='Matteo Bordignon',
     maintainer_email='matteo.bordignon@polimi.it',
     description='ROS2 driver for TeraRanger Evo sensor',
-    license='TODO: License declaration',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
